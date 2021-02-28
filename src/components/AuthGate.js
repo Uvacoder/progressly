ff3d42b2173d7764d33f2ee85ba2ef9b96fcae7a
mbrodt <madsbrodt3@gmail.com>
2021-02-28 02:09:52 +0100

    Remove unused files

--- src/components/AuthGate.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
-
-export default function AuthGate() {
-  const isSignIn = true;
-  return <div>{isSignIn ? <SignIn /> : <SignUp />}</div>;
-}
