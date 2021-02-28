497628420ae68be8c380f0f74a9cb6ec7b12c1ff
mbrodt <madsbrodt3@gmail.com>
2021-02-28 02:09:21 +0100

    Highlight active route in Layout and change user snippet in bottom

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,137 +1,14 @@
 import LinkTo from "../components/LinkTo.js";
 import { useAuth } from "../authProvider";
+import { useRouter } from "next/router";
+
 export default function Layout({ children }) {
   const { user, signout } = useAuth();
+  const router = useRouter();
+  const pathname = router.pathname;
+  console.log("user in LAYOUT:", user);
   return (
     <div className="h-screen flex overflow-hidden bg-white">
-      {/* <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. --> */}
-      {/* <div className="lg:hidden">
-        <div className="fixed inset-0 flex z-40">
-          <div className="fixed inset-0">
-            <div className="absolute inset-0 bg-gray-600 opacity-75"></div>
-          </div>
-          <div
-            tabIndex="0"
-            className="relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none"
-          >
-            <div className="absolute top-0 right-0 -mr-12 pt-2">
-              <button
-                type="button"
-                className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              >
-                <span className="sr-only">Close sidebar</span>
-
-                <svg
-                  className="h-6 w-6 text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-              <div className="flex-shrink-0 flex items-center px-4">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-900-text.svg"
-                  alt="Workflow"
-                />
-              </div>
-              <nav aria-label="Sidebar" className="mt-5">
-                <div className="px-2 space-y-1">
-                  <LinkTo
-                    to="/"
-                    className="bg-gray-100 text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                  >
-                    <>
-                      {" "}
-                      <svg
-                        className="text-gray-500 mr-4 h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                        />
-                      </svg>
-                      Explore
-                    </>
-                  </LinkTo>
-
-                  <LinkTo
-                    to="/my-profile"
-                    className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                  >
-                    <>
-                      {" "}
-                      <svg
-                        className="text-gray-400 group-hover:text-gray-500 mr-4 h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                        />
-                      </svg>
-                      My Profile
-                    </>
-                  </LinkTo>
-                </div>
-              </nav>
-            </div>
-            <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
-              <a href="#" className="flex-shrink-0 group block">
-                <div className="flex items-center">
-                  <div>
-                    <img
-                      className="inline-block h-10 w-10 rounded-full"
-                      src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    {user && (
-                      <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
-                        {user?.displayName}
-                      </p>
-                    )}
-                    <button
-                      onClick={() => signout()}
-                      className="text-sm font-medium text-gray-500 group-hover:text-gray-700"
-                    >
-                      Log out
-                    </button>
-                  </div>
-                </div>
-              </a>
-            </div>
-          </div>
-          <div className="flex-shrink-0 w-14" aria-hidden="true">
-          </div>
-        </div>
-      </div> */}
-
       {/* <!-- Static sidebar for desktop --> */}
       <div className="hidden lg:flex lg:flex-shrink-0">
         <div className="flex flex-col w-64">
@@ -150,38 +27,51 @@ export default function Layout({ children }) {
                   {/* <!-- Current: "bg-gray-200 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" --> */}
                   <LinkTo
                     to="/"
-                    className="bg-gray-200 text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className={`group flex items-center px-2 py-2 text-sm font-medium rounded-md ${
+                      pathname === "/"
+                        ? "bg-gray-200 text-gray-900"
+                        : "text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                    }`}
                   >
                     {/* <!-- Current: "text-gray-500", Default: "text-gray-400 group-hover:text-gray-500" --> */}
                     {/* <!-- Heroicon name: outline/home --> */}
-                    <>
-                      <svg
-                        className="text-gray-500 mr-3 h-6 w-6"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                        />
-                      </svg>
-                      Explore
-                    </>
+                    <svg
+                      className={`mr-3 h-6 w-6 ${
+                        pathname === "/"
+                          ? "text-gray-500"
+                          : "text-gray-400 group-hover:text-gray-500"
+                      }`}
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                      />
+                    </svg>
+                    Explore
                   </LinkTo>
 
                   <LinkTo
                     to="/my-profile"
-                    className="text-gray-600 hover:bg-gray-50 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                    className={`group flex items-center px-2 py-2 text-sm font-medium rounded-md ${
+                      pathname === "/my-profile"
+                        ? "bg-gray-200 text-gray-900"
+                        : "text-gray-600 hover:bg-gray-50 hover:text-gray-900"
+                    }`}
                   >
-                    {/* <!-- Heroicon name: outline/calendar --> */}
                     <>
                       <svg
-                        className="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"
+                        className={`mr-3 h-6 w-6 ${
+                          pathname === "/my-profile"
+                            ? "text-gray-500"
+                            : "text-gray-400 group-hover:text-gray-500"
+                        }`}
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
@@ -202,33 +92,40 @@ export default function Layout({ children }) {
               </nav>
             </div>
             <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
-              <a href="#" className="flex-shrink-0 w-full group block">
-                <div className="flex items-center">
-                  <div>
-                    <img
-                      className="inline-block h-9 w-9 rounded-full"
-                      src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </div>
-                  <div className="ml-3">
-                    {user && (
+              {user ? (
+                <LinkTo
+                  to="/my-profile"
+                  className="flex-shrink-0 w-full group block"
+                >
+                  <div className="flex items-center">
+                    <div>
+                      <img
+                        className="inline-block h-9 w-9 rounded-full"
+                        src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
+                        alt=""
+                      />
+                    </div>
+                    <div className="ml-3">
                       <p className="text-base font-medium text-gray-700 group-hover:text-gray-900">
-                        {user?.displayName}{" "}
-                        <span className="text-xs text-gray-600">
-                          ({user?.email})
-                        </span>
+                        {user?.displayName}
                       </p>
-                    )}
-                    <button
-                      onClick={() => signout()}
-                      className="text-sm font-medium text-gray-500 group-hover:text-gray-700"
-                    >
-                      Log out
-                    </button>
+                      <button
+                        onClick={() => signout()}
+                        className="text-sm font-medium text-gray-500 hover:text-gray-700"
+                      >
+                        Log out
+                      </button>
+                    </div>
                   </div>
-                </div>
-              </a>
+                </LinkTo>
+              ) : (
+                <LinkTo
+                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  to="/sign-in"
+                >
+                  Sign In
+                </LinkTo>
+              )}
             </div>
           </div>
         </div>
