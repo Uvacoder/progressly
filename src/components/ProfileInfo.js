6ee14db04c11c1c8d8f69d8cdbe7fcbc2dca84d1
mbrodt <madsbrodt3@gmail.com>
2021-03-07 01:53:59 +0100

    Add Tag functionality

--- src/components/ProfileInfo.js
+++ src/components/ProfileInfo.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { useAuth } from "../authProvider";
 import ProfileTags from "./ProfileTags";
-export default function ProfileInfo({ displayName, email, tags }) {
-  console.log("tags:", tags);
+export default function ProfileInfo({ displayName, email }) {
   // const { user } = useAuth();
   return (
     <div>
       <h1 className="text-5xl font-bold">Hello {displayName}!</h1> Your email is{" "}
       {email}!
-      <ProfileTags existingTags={tags} />
+      <ProfileTags />
     </div>
   );
 }
--- src/components/ProfileTags.js
+++ src/components/ProfileTags.js
@@ -1,70 +1,103 @@
 import { useState, useEffect } from "react";
+const colors = require("tailwindcss/colors");
+
 import { db } from "../initFirebase";
 import { useAuth } from "../authProvider";
+import Input from "./ui/Input";
 
-export default function ProfileTags({ existingTags }) {
+export default function ProfileTags() {
   const { user } = useAuth();
-
-  console.log("tags in profiletags:", existingTags);
-  const [input, setInput] = useState("");
-  const [tags, setTags] = useState(existingTags || []);
+  const [tag, setTag] = useState("");
+  const [tags, setTags] = useState({});
 
   useEffect(() => {
-    if (existingTags) {
-      setTags(existingTags);
-    }
-  }, [existingTags]);
-  useEffect(() => {
-    console.log("tags changed");
-    // return () => {
-    // 	cleanup
-    // }
-    if (tags && tags.length > 0) {
-      console.log("updating tags", tags);
-      db.ref("users/" + user.displayName).update({
-        tags: tags,
+    // Get all tags on the user
+    const userTags = db.ref(`users/${user.displayName}/tags`);
+    const refs = [userTags];
+    // When a child is added to the user tags (through the submitTag function, or on mount), we update the client side tags with the old ones, plus the newly added tag
+    userTags.on("child_added", (child) => {
+      const key = child.key;
+      const tagRef = db.ref(`tags/${key}`);
+      refs.push(tagRef);
+      tagRef.on("value", (snap) => {
+        setTags((old) => {
+          return {
+            ...old,
+            [key]: snap.val(),
+          };
+        });
       });
-    }
-  }, [tags]);
+    });
+    return () => {
+      refs.forEach((ref) => ref.off());
+    };
+  }, []);
 
-  const removeTag = (i) => {
-    const newTags = [...tags];
-    newTags.splice(i, 1);
+  const removeTag = (tagId) => {
+    const userTags = db.ref(`users/${user.displayName}/tags/${tagId}`);
+    const tagRef = db.ref(`tags/${tagId}`);
+    userTags.remove();
+    tagRef.remove();
+
+    const newTags = tags;
+    delete newTags[tagId];
     setTags(newTags);
   };
 
-  const onKeyDown = (e) => {
-    console.log("e:", e);
-    if (e.key === "Enter" && input) {
-      if (tags.find((tag) => tag.toLowerCase() === input.toLowerCase())) {
-        return;
-      }
-      setTags([...tags, input]);
-      return setInput("");
+  const onSubmitTag = async (e) => {
+    e.preventDefault();
+    if (!tag) return;
+    if (Object.keys(tags).length === 5) return;
+    for (const val of Object.values(tags)) {
+      if (val.tag.toLowerCase() === tag.toLowerCase()) return;
     }
+
+    const tagRef = db.ref("tags").push({
+      tag,
+      displayName: user.displayName,
+    });
+    await db.ref(`users/${user.displayName}/tags/${tagRef.key}`).set(true);
+    return setTag("");
   };
   return (
     <div>
-      <p>Profile Tags</p>
       <ul>
-        {tags.map((tag, i) => {
+        {Object.entries(tags).map(([id, tag]) => {
           return (
-            <li key={tag}>
-              <span>{tag}</span>
-              <button onClick={() => removeTag(i)}>X</button>
-            </li>
+            <Badge key={id} id={id} name={tag.tag} removeTag={removeTag} />
           );
         })}
-        <li>
-          <input
+        <form className="max-w-xs my-4" onSubmit={onSubmitTag}>
+          <Input
             type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={onKeyDown}
+            value={tag}
+            placeholder="Add your main interest areas (maximum of 5)"
+            onChange={(e) => setTag(e.target.value)}
           />
-        </li>
+        </form>
       </ul>
-      {input}
     </div>
   );
 }
+
+const Badge = ({ id, name, removeTag }) => {
+  return (
+    <li className="inline-flex rounded-full items-center py-1 pl-2.5 pr-2 text-lg font-medium bg-green-100 text-green-800">
+      {name}
+      <button
+        type="button"
+        onClick={() => removeTag(id)}
+        className="flex-shrink-0 ml-1 h-4 w-4 rounded-full inline-flex items-center justify-center text-green-300 hover:bg-green-200 hover:text-green-500 focus:outline-none focus:bg-green-500 focus:text-white"
+      >
+        <svg
+          className="h-3 w-3"
+          stroke="currentColor"
+          fill="none"
+          viewBox="0 0 8 8"
+        >
+          <path strokeLinecap="round" strokeWidth="1.5" d="M1 1l6 6m0-6L1 7" />
+        </svg>
+      </button>
+    </li>
+  );
+};
